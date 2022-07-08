7231d2097b51fe59da8d2f1cba16fd566740e8f9
markosavic14 <markosavic14@gmail.com>
2022-07-08 19:10:54 +0200

    added trackbars

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
 #include <unistd.h>
@@ -31,13 +32,42 @@ using namespace cv;
 #define BLUE_V_MIN 77
 #define BLUE_V_MAX 255
 
-//WHITE
-#define WHITE_H_MIN 0
-#define WHITE_H_MAX 180
-#define WHITE_S_MIN 0
-#define WHITE_S_MAX 18
-#define WHITE_V_MIN 231
-#define WHITE_V_MAX 255
+//YELLOW
+#define YELLOW_H_MIN 15
+#define YELLOW_H_MAX 55
+#define YELLOW_S_MIN 100
+#define YELLOW_S_MAX 255
+#define YELLOW_V_MIN 100
+#define YELLOW_V_MAX 255
+
+//GREEN
+#define GREEN_H_MIN 50
+#define GREEN_H_MAX 100
+#define GREEN_S_MIN 100
+#define GREEN_S_MAX 255
+#define GREEN_V_MIN 100
+#define GREEN_V_MAX 255
+
+
+
+
+Mat findColor(Mat img, int hmin1, int hmax1, int hmin2, int hmax2, int smin, int smax, int vmin, int vmax){
+
+	Mat hsvMask, hsvMask1, hsvMask2, imgHSV;
+
+	cvtColor(img,imgHSV,COLOR_BGR2HSV);//konverzija u hsv prostor
+
+	Scalar lower_1(hmin1, smin, vmin);
+	Scalar upper_1(hmax1, smax, vmax);
+	Scalar lower_2(hmin2, smin, vmin);
+	Scalar upper_2(hmax2, smax, vmax);
+	inRange(imgHSV,lower_1,upper_1,hsvMask1);
+	inRange(imgHSV, lower_2, upper_2, hsvMask2);
+
+	hsvMask=hsvMask1 | hsvMask2;
+
+	return hsvMask;
+}
 
 
 Mat getEdges(Mat hsvMask){
@@ -50,6 +80,8 @@ Mat getEdges(Mat hsvMask){
 
 	return imgCanny;
 }
+
+int cut_size = 0;
 
 void getContours(Mat ivice, Mat original){
 	vector<vector<Point>> contours;
@@ -87,16 +119,18 @@ void getContours(Mat ivice, Mat original){
 			normalize(mask.clone(), mask, 0.0, 255.0, CV_MINMAX, CV_8UC1);
 
 			Mat imgcrop=crop(boundRect[i]);
-			imshow("cut"+ to_string(i),imgcrop);
+			imshow("cut"+ to_string(cut_size),imgcrop);
+			cut_size += 1;
 			imshow("Slika sa maskiranom pozadinom",crop);
 		}
-		cout<<endl<<endl<<endl;
+		cout<<endl<<endl;
 	}
 }
 
 
 int main() {
-	Mat src, ImgOriginal, imgCanny, imgHSV, hsvMask, red_hsvMask1, red_hsvMask2, blue_hsvMask, white_hsvMask;
+	Mat src, ImgOriginal, imgCanny, hsvMask;
+	int hmin1 = RED_H_MIN_1, hmax1 = RED_H_MAX_1, hmin2 = RED_H_MIN_2, hmax2 = RED_H_MAX_2, smin = RED_S_MIN, smax = RED_S_MAX, vmin = RED_V_MIN, vmax = RED_V_MAX;
 	
 	//blue
 	//src = cv::imread("data/blue/crosswalk.jpg");
@@ -108,7 +142,7 @@ int main() {
 	//src = cv::imread("data/green/highway_exit.jpeg");
 
 	//red
-	src = cv::imread("data/red/no_parking.jpg");
+	//src = cv::imread("data/red/no_parking.jpg");
 	//src = cv::imread("data/red/no_priority.jpg");
 	//src = cv::imread("data/red/stop_sign.jpg");
 	//src = cv::imread("data/red/stop.jpg");
@@ -119,41 +153,97 @@ int main() {
 	//src = cv::imread("data/white/priority.jpg");
 
 	//mixed
-	//src = cv::imread("data/mixed/Auto-put-petlja-Nis-jug.jpg");
+	src = cv::imread("data/mixed/Auto-put-petlja-Nis-jug.jpg");
 	//src = cv::imread("data/mixed/pedestrian_zone.jpg");
 	//src = cv::imread("data/mixed/procced_straight.jpg");
 
 		
 	if(src.empty()) throw runtime_error("Cannot open image!");
-	cout<<"Img size:"<<src.size()<<endl;
-	resize(src,ImgOriginal,Size(),0.5,0.5);//neutral
-	cvtColor(ImgOriginal,imgHSV,COLOR_BGR2HSV);//konverzija u hsv prostor
+	cout<<endl<<"src size:"<<src.size().width<<endl;
+
+	namedWindow("Trackbars", (200, 200));
+
+	string s_w = "0 : RED\n1 : BLUE\n2 : GREEN\n3 : YELLOW";
+	createTrackbar(s_w, "Trackbars", 0, 3);
+	setTrackbarPos(s_w, "Trackbars", 0);
+
+	string r_w = "Resize";
+	createTrackbar(r_w, "Trackbars", 0, 10);
+	setTrackbarPos(r_w, "Trackbars", 5);
 
 
-	Scalar red_lower_1(RED_H_MIN_1,RED_S_MIN,RED_V_MIN);
-	Scalar red_upper_1(RED_H_MAX_1,RED_S_MAX,RED_V_MAX);
-	inRange(imgHSV,red_lower_1,red_upper_1,red_hsvMask1);
+	while(true){
+		int s_b = getTrackbarPos(s_w, "Trackbars");
 
-	Scalar red_lower_2(RED_H_MIN_2,RED_S_MIN,RED_V_MIN);
-	Scalar red_upper_2(RED_H_MAX_2,RED_S_MAX,RED_V_MAX);
-	inRange(imgHSV,red_lower_2,red_upper_2,red_hsvMask2);
+		switch(s_b){
+			case 0: //red
+				hmin1 = RED_H_MIN_1;
+				hmax1 = RED_H_MAX_1;
+				hmin2 = RED_H_MIN_2;
+				hmax2 = RED_H_MAX_2;
+				smin = RED_S_MIN;
+				smax = RED_S_MAX;
+				vmin = RED_V_MIN;
+				vmax = RED_V_MAX;
+				break;
+			case 1://blue
+				hmin1 = BLUE_H_MIN;
+				hmax1 = BLUE_H_MAX;
+				hmin2 = 0;
+				hmax2 = 0;
+				smin = BLUE_S_MIN;
+				smax = BLUE_S_MAX;
+				vmin = BLUE_V_MIN;
+				vmax = BLUE_V_MAX;
+				break;
+			case 2://green
+				hmin1 = GREEN_H_MIN;
+				hmax1 = GREEN_H_MAX;
+				hmin2 = 0;
+				hmax2 = 0;
+				smin = GREEN_S_MIN;
+				smax = GREEN_S_MAX;
+				vmin = GREEN_V_MIN;
+				vmax = GREEN_V_MAX;
+				break;
+			case 3://yellow
+				hmin1 = YELLOW_H_MIN;
+				hmax1 = YELLOW_H_MAX;
+				hmin2 = 0;
+				hmax2 = 0;
+				smin = YELLOW_S_MIN;
+				smax = YELLOW_S_MAX;
+				vmin = YELLOW_V_MIN;
+				vmax = YELLOW_V_MAX;
+				break;
+			default:
+				break;
+		}
 
-	Scalar blue_lower(BLUE_H_MIN,BLUE_S_MIN,BLUE_V_MIN);
-	Scalar blue_upper(BLUE_H_MAX,BLUE_S_MAX,BLUE_V_MAX);
-	inRange(imgHSV,blue_lower,blue_upper,blue_hsvMask);
+		int r_b = getTrackbarPos(r_w, "Trackbars") + 1;
 
-	// Scalar white_lower(WHITE_H_MIN, WHITE_S_MIN, WHITE_V_MIN);
-	// Scalar white_upper(WHITE_H_MAX, WHITE_S_MAX, WHITE_V_MAX);
-	// inRange(imgHSV,white_lower,white_upper,white_hsvMask);;
+		resize(src,ImgOriginal,Size(), r_b*0.2, r_b*0.2);
+	
+		hsvMask = findColor(ImgOriginal, hmin1, hmax1, hmin2, hmax2, smin, smax, vmin, vmax);
 
-	hsvMask=red_hsvMask1 | red_hsvMask2 | blue_hsvMask;// od vise delova maske dobijamo jednu
+		imgCanny = getEdges(hsvMask);
 
-	imgCanny = getEdges(hsvMask);
-	imshow("Canny", imgCanny);
-	getContours(imgCanny, ImgOriginal);
+		getContours(imgCanny, ImgOriginal);
 
 
-	imshow("Image",ImgOriginal);
-	waitKey(0);
+		imshow("Originalna slika",ImgOriginal);
+		imshow("Canny", imgCanny);
+		waitKey(0);
+		destroyWindow("Originalna slika");
+		destroyWindow("Canny");
+		destroyWindow("Slika sa maskiranom pozadinom");
+		while(cut_size >= 0){
+			destroyWindow("cut"+ to_string(cut_size));
+			cut_size -= 1;
+		}
+		cut_size = 0;
+	}
+
+
 	return 0;
 }
